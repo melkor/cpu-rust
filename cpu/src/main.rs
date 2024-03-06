a02fd6e3f9313c61277660a9e33e996b9e8b427f
jerome bourgeois <jerome.bourgeois@adevinta.com>
2024-03-06 10:52:10 +0100

    handle noop and halt

--- cpu/src/main.rs
+++ cpu/src/main.rs
@@ -1,56 +1,74 @@
+use std::collections::HashMap;
 use std::fs::File;
-use std::io::{self, Read};
+use std::io::{self, BufRead};
 use std::process;
 
-enum OpCodes {
-    OpNop,
-    OpHalt,
-    OpMov,
-    OpAdd,
+
+static OP_MASK:i64 = 0b11111111;
+static OP_NOOP:i8 = 0b00000000;
+static OP_HALT:i8 = 0b00000001;
+static OP_MOV:i8 = 0b00000010;
+static OP_ADD:i8 = 0b00000011;
+
+
+fn load_code(file_name: &str) -> io::Result<io::Lines<io::BufReader<File>>> {
+    let fh = File::open(file_name)?;
+    Ok(io::BufReader::new(fh).lines())
 }
 
-fn load_code(file_name: &str) -> Result<String, io::Error> {
-    let mut code = String::new();
-    File::open(file_name)?.read_to_string(&mut code)?;
-    Ok(code)
+// 4 bits: opcode 
+// |0000| 
+fn decode(line: &str, op_list: &HashMap<&str, i8>) -> Result<i64, String> {
+    let mut inst:i64 = 0;
+    for token in line.split_whitespace() {
+        match op_list.get(token) {
+            Some(op) => inst = inst | i64::from(op.clone()),
+            _ => return Err(format!("Unsupported OP: {}", token)),
+        }
+    }
+    Ok(inst)
 }
 
 fn main() {
-    let _reg_eax = "";
-    let _reg_ebx = "";
-    let _reg_ecx = "";
-    let _reg_edx = "";
+    let _op_list = HashMap::from([
+        ("NOOP", 0b0),
+        ("HALT", 0b1),
+    ]);
 
-    let op_code = OpCodes::OpNop;
+    let mut _reg_rax: i64 = 0;
+    let mut _reg_rbx: i64 = 0;
+    let mut _reg_rcx: i64 = 0;
+    let mut _reg_rdx: i64 = 0;
 
-    let code = match load_code("code.rsm") {
+    let code_reader = match load_code("code.rsm") {
         Ok(content) => content,
         Err(err) => {
             eprintln!("Problem parsing code: {err}");
             process::exit(2);
-        },
+        }
     };
 
-    let mut current_inst = String::new();
+    let mut reg_inst: i64 = 0;
+    for line in code_reader.flatten() {
+        match decode(&line, &_op_list) {
+            Err(err) => {
+                eprintln!("decoding error: {}", err);
+                process::exit(1);
+            },
+            Ok(inst) => reg_inst = inst,
+        }
+        println!("{}", reg_inst);
 
-    for c in code.chars() {
-        if c != '\n' {
-            current_inst.push(c);
+        let op_code = (reg_inst & OP_MASK) as i8;
+        println!("op: {}", reg_inst);
+
+        if op_code == OP_NOOP {
+            println!("no op");
+        } else if op_code == OP_HALT {
+            println!("halt!!");
+            process::exit(0);
         }
     }
-
-    println!("{}", current_inst);
-
-    match op_code {
-        OpCodes::OpNop => println!("no op"),
-        OpCodes::OpHalt => { 
-            println!("halt");
-            process::exit(0);
-        },
-        OpCodes::OpMov => println!("move"),
-        OpCodes::OpAdd => println!("add"),
-    }
-    
 
     println!("Hello, world!");
 }
